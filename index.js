0c41be06149f15ac58daad725e360440cf41c0b1
Richard <richardprax2003@gmail.com>
2024-10-14 17:25:10 +0200

    feat: small fix

--- index.js
+++ index.js
@@ -59,8 +59,12 @@ app.post('/cars/:id/passengers', async (req, res) => {
     }
 });
 
+app.get("/", (req, res) => { res.send("Express on Vercel"); });
+
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;

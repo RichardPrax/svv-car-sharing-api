44cab6176d4b9888c11a9d3dbc5d665fe0ae9fd9
Richard <you@example.com>
2024-10-16 10:50:06 +0200

    feat: short test

--- routes/gameDays.js
+++ routes/gameDays.js
@@ -14,7 +14,7 @@ router.get('/', auth, async (req, res) => {
 
 router.get('/:id', auth, async (req, res) => {
     try {
-        const gameDay = await GameDay.findById(req.params.id).populate('cars');
+        const gameDay = await GameDay.findById(req.params.id).populate('cars registeredUsers');
         if (!gameDay) {
             return res.status(404).json({ message: 'Spieltag nicht gefunden' });
         }

f500bb1c4ef1ec200a2657d82ba67b8c09bfc2ad
Richard <you@example.com>
2024-10-17 09:49:03 +0200

    feat: updated car model

--- models/Car.js
+++ models/Car.js
@@ -4,10 +4,11 @@ const carSchema = new mongoose.Schema({
     owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     numberOfSeats: { type: Number, required: true },
     driver: { type: String, required: true },
-    departureTime: { type: Date, required: true },
+    departureTime: { type: String, required: true },
     departureFrom: { type: String, required: true },
     registeredUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    gameDay: { type: mongoose.Schema.Types.ObjectId, ref: 'GameDay', required: true }
+    gameDay: { type: mongoose.Schema.Types.ObjectId, ref: 'GameDay', required: true },
+    info: {type: String, required: false},
 }, { timestamps: true });
 
 module.exports = mongoose.model('Car', carSchema);

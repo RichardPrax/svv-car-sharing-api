a45c7dc22c4192ef595c25f8aa5e4f42f3261b44
Richard <richardprax2003@gmail.com>
2025-01-10 15:05:10 +0100

    removed password from login

--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-router.post('/register', async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        const user = new User({ username, password });
-        await user.save();
-        res.status(201).json({ message: 'User registered successfully' });
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = new User({ username, password });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
-router.post('/authenticate', async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        const user = await User.findOne({ username });
-        if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+router.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
-        const isMatch = await user.comparePassword(password);
-        if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+    // NOTE: removed password, because no body wants to enter it
+    // const isMatch = await user.comparePassword(password);
+    // if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-        const jwtToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const jwtToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
 
-        res.json({ username: user.username, id: user._id, accessToken: jwtToken });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+    res.json({ username: user.username, id: user._id, accessToken: jwtToken });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
-
 
 module.exports = router;
